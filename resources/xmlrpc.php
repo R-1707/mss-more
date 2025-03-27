b796ff3282e639fde8c8454053e3a2545869f0bf
Ryan Gascoigne-Jones <rtg204@exeter.ac.uk>
2025-03-27 23:33:37 +0000

    Switched to local dependencies

---
+++ resources/xmlrpc.php
@@ -0,0 +1,14 @@
+<rsd xmlns="http://archipelago.phrasewise.com/rsd" version="1.0">
+<service>
+<engineName>WordPress</engineName>
+<engineLink>https://wordpress.org/</engineLink>
+<homePageLink>http://www.moreshapesservices.co.uk</homePageLink>
+<apis>
+<api name="WordPress" blogID="1" preferred="true" apiLink="./resources/xmlrpc.php"/>
+<api name="Movable Type" blogID="1" preferred="false" apiLink="./resources/xmlrpc.php"/>
+<api name="MetaWeblog" blogID="1" preferred="false" apiLink="./resources/xmlrpc.php"/>
+<api name="Blogger" blogID="1" preferred="false" apiLink="./resources/xmlrpc.php"/>
+<api name="WP-API" blogID="1" preferred="false" apiLink="./resources/site_json.xml"/>
+</apis>
+</service>
+</rsd>
